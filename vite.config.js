4efe6e1e635d062289fb50f80bb9284ebcef64ec
Totance <tance@lingx.ai>
2026-04-21 19:52:19 +0800

    fix: Remove workflow files to fix push

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glsl', '**/*.svg'],
+  base: '/lingx-website-v2/',
+  build: {
+    outDir: 'docs',
+  },
 })
